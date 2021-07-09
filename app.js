9c8f10e1a899cf93296da8cb2270aec9df6eef64
jyiwm <joshua.yi@warnermedia.com>
2021-07-09 12:26:01 -0400

    Add conditional to send form data to server

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><titleEnter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="datadata"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();  // have to return so that we can't call res.write anymore
+    }
     console.log(req.url, req.method, req.headers);
     // process.exit();
     res.setHeader('Content-Type', 'text/html');
