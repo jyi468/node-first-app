2a39d3e3d758e6e74bca58d8e99158973d8cde3e
jyiwm <joshua.yi@warnermedia.com>
2021-07-12 15:07:10 -0400

    Add middleware and use next to go to next middleware

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const http = require('http');
 
+// const routes = require('./routes'); // local module, so we need to relative path syntax. don't need '.js'
+// const server = http.createServer(routes);
+
 const express = require('express');
 
 const app = express(); // initialize a new express object which is a valid request handler
 
 const server = http.createServer(app);
 
-// const routes = require('./routes'); // local module, so we need to relative path syntax. don't need '.js'
-// const server = http.createServer(routes);
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    // next function must be executed so next middleware is run
+    next();
+}); // executed for every incoming request
+
+app.use((req, res, next) => {
+    console.log("In another middleware!");
+    // send response if no more middleware
+});
 
 // Nodejs keeps server listening for incoming requests
 server.listen(3000);
