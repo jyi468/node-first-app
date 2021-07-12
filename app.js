0c43aedb70b8fd9ffa7486939cafc9af30da4b8b
jyiwm <joshua.yi@warnermedia.com>
2021-07-12 15:55:56 -0400

    Add body parser for incoming requests and responses

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const http = require('http');
+// const http = require('http');
 
 // const routes = require('./routes'); // local module, so we need to relative path syntax. don't need '.js'
 // const server = http.createServer(routes);
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express(); // initialize a new express object which is a valid request handler
 
-app.use('/', (req, res, next) => {
-    console.log("This always runs");
-    next();
+// Order of middlewares is important because '/' path match handles every combination
+app.use(bodyParser.urlencoded({extended: false}));  // parses body and calls next afterward. Parses body for things like form data
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-// Order of middlewares is important because '/' path match handles every combination
-app.use('/add-product', (req, res, next) => {
-    console.log("In products");
-    res.send('<h1>Product</h1>');
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
